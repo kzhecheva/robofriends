13e727edd4b825440e395aae7b3cf638af1f29fd
Kalina Zhecheva <zhecheva.kalina@gmail.com>
2020-11-03 17:10:31 +0200

    update project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'tralala@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ognqna Raikova',
+        username: 'Ema',
+        email: 'eho@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Tirilqm Orjem',
+        username: 'Oleg',
+        email: 'off@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Barbara Stainberg',
+        username: 'Mauia',
+        email: 'bau@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Karmela Lindi',
+        username: 'Ofena',
+        email: 'ihy@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Petar Mladenov',
+        username: 'Phill',
+        email: 'olele@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Oleg Mihailovski',
+        username: 'Koren',
+        email: 'pupuf@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Paula Gorbachov',
+        username: 'Polly',
+        email: 'papas@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Katalina Furre',
+        username: 'Kelly',
+        email: 'tatas@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Melinda Bea',
+        username: 'Molly',
+        email: 'mapas@gmail.com'
+    },
+];
