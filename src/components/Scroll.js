e678fa803adcb707201df66c6c3f65d2ffb8d084
Kalina Zhecheva <zhecheva.kalina@gmail.com>
2020-11-04 11:28:16 +0200

    scroll height fix

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', height: '1200px' }}>
+        <div style={{ overflowY: 'scroll', height: '900px' }}>
             {props.children}
         </div>
     )
